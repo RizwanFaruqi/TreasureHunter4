e078117f77b430265ed854807d1e0a1185d1e435
BT_1N3_22 <rizwanf@nycstudents.net>
2025-01-14 13:48:07 -0500

    1/14 first commit

--- src/Hunter.java
+++ src/Hunter.java
@@ -18,7 +18,7 @@ public class Hunter {
      */
     public Hunter(String hunterName, int startingGold) {
         this.hunterName = hunterName;
-        kit = new String[5]; // only 5 possible items can be stored in kit
+        kit = new String[6]; // only 6 possible items can be stored in kit
         gold = startingGold;
     }
 
@@ -201,6 +201,7 @@ public class Hunter {
         addItem("Machete");
         addItem("Horse");
         addItem("Boat");
+        addItem("Boots");
         return true;
     }
 }
